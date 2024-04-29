1e04d9f1ebd7fd087fac6264ce4dcd489dbfc9dc
Tyler Maguire <tyler.maguire@epiuse.com>
2024-04-29 13:09:11 +0000

    yt

--- compare/webapp/controller/appView.controller.js
+++ compare/webapp/controller/appView.controller.js
@@ -14,6 +14,28 @@ sap.ui.define([
                 if (oEvent.getParameters("files")) {
                     var file = oEvent.getParameters("files").files[0]; 
                 }
+
+        },
+            handleUploadComplete1s: function(oEvent) {
+                if (oEvent.getParameters("files")) {
+                    var file = oEvent.getParameters("files").files[0]; 
+                }
+            },
+            handleUploadComplete2: function(oEvent) {
+                if (oEvent.getParameters("files")) {
+                    var file = oEvent.getParameters("files").files[0]; 
+                }
+            }, 
+    
+            handleUploadPress1: function() {
+                var oFileUploader1 = this.byId("fileUploader1");
+                oFileUploader1.checkFileReadable().then(function() {
+                 
+                var oFileUploader1 = this.byId("fileUploader1");
+                var domRef = oFileUploader1.getFocusDomRef();
+                var file = domRef.files[0];
+                var reader = new FileReader();
+
                 reader.onload = function(oEvent) {
                     var strCSV = oEvent.target.result;
                     var arrCSV = strCSV.match(/[\w .]+(?=,?)/g);
@@ -36,22 +58,7 @@ sap.ui.define([
                     }
                     params = params.substring(0, params.length - 2);
                     params += "]";
-            },
-            handleUploadComplete1s: function(oEvent) {
-                if (oEvent.getParameters("files")) {
-                    var file = oEvent.getParameters("files").files[0]; 
-                }
-            },
-            handleUploadComplete2: function(oEvent) {
-                if (oEvent.getParameters("files")) {
-                    var file = oEvent.getParameters("files").files[0]; 
-                }
-            }, 
-    
-            handleUploadPress1: function() {
-                var oFileUploader1 = this.byId("fileUploader1");
-                oFileUploader1.checkFileReadable().then(function() {
-                    oFileUploader1.upload();
+            }
                 }, function(error) {
                     MessageToast.show("File 1 cannot be read. It may have changed.");
                 }).then(function() {
@@ -61,7 +68,35 @@ sap.ui.define([
             handleUploadPress2: function() {
                 var oFileUploader2 = this.byId("fileUploader2");
                 oFileUploader2.checkFileReadable().then(function() {
-                    oFileUploader2.upload();
+                    
+                var oFileUploader1 = this.byId("fileUploader2");
+                var domRef = oFileUploader1.getFocusDomRef();
+                var file = domRef.files[0];
+                var reader = new FileReader();
+
+                reader.onload = function(oEvent) {
+                    var strCSV = oEvent.target.result;
+                    var arrCSV = strCSV.match(/[\w .]+(?=,?)/g);
+                    var noOfCols = 6;
+                    var headerRow = arrCSV.splice(0, noOfCols);
+                    var data = [];
+                    while (arrCSV.length > 0) {
+                      var obj = {};
+                      var row = arrCSV.splice(0, noOfCols);
+                      for (var i = 0; i < row.length; i++) {
+                        obj[headerRow[i]] = row[i].trim();
+                      }
+                      data.push(obj);
+                    }
+                    var Len = data.length;
+                    data.reverse();
+                    params += "[";
+                    for (var j = 0; j < Len; j++) {
+                      params += JSON.stringify(data.pop()) + ", ";
+                    }
+                    params = params.substring(0, params.length - 2);
+                    params += "]";
+            }
                 }, function(error) {
                     MessageToast.show("File 2 cannot be read. It may have changed.");
                 }).then(function() {
